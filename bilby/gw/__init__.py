0cf88ab68fd9f98e4c332ad80187ca0212875a6a
rajendra-bhatt <bhattrajendra1997@gmail.com>
2026-06-08 13:23:35 +0530

    Clean up imports in __init__.py
    Removed 'source_TH' import from __init__.py.

--- bilby/gw/__init__.py
+++ bilby/gw/__init__.py
@@ -1,5 +1,5 @@
 from . import (conversion, cosmology, detector, eos, likelihood, prior,
-               result, source, utils, waveform_generator,source_TH)
+               result, source, utils, waveform_generator)
 from .waveform_generator import WaveformGenerator, LALCBCWaveformGenerator
 from .likelihood import GravitationalWaveTransient
 from .detector import calibration
